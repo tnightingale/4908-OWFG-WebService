48b677918353066235c06f99e0452b9304432413
Tom Nightingale <tom@tnightingale.com>
2011-05-11 19:46:11 -0700

    more cleanup

--- WebService/src/main/java/com/owfg/facade/ws/StoreManagement/StoreManagementImpl.java
+++ WebService/src/main/java/com/owfg/facade/ws/StoreManagement/StoreManagementImpl.java
@@ -2,25 +2,20 @@ package com.owfg.facade.ws.StoreManagement;
 
 import javax.jws.WebParam;
 import javax.jws.WebService;
-import javax.jws.WebMethod;
-import java.util.ArrayList;
 import java.util.List;
 
 @WebService
 public class StoreManagementImpl implements StoreManagement {
     private Factory factory = new Factory();
 
-    @WebMethod
     public List<Store> getActiveStores() {
         return factory.getStores();
     }
 
-    @WebMethod
     public List<Banner> getBanners() {
         return factory.getBanners();
     }
 
-    @WebMethod
     public StoreManagementInfo getStoreManagementInfo(@WebParam(name = "storeId", targetNamespace = "") long storeId, @WebParam(name = "upc", targetNamespace = "") String upc) {
         return factory.getStoreManagementInfo(storeId, upc);
     }
